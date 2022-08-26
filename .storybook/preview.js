51c867012e363c9c538d25406986d0496e4304aa
woose28 <dldntmdsla@naver.com>
2022-08-26 11:53:06 +0900

    chore: storybook 추가 설정
    - 절대 경로 설정
    - 조절이 필요 없는 props control disabled 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const argTypes = {
+  theme: {
+    table: {
+      disable: true,
+    },
+  },
+  as: {
+    table: {
+      disable: true,
+    },
+  },
+};
