acd07d38ff58763aaf1a90e42536fcc6d3e8531d
woose28 <dldntmdsla@naver.com>
2022-08-26 10:51:25 +0900

    feat: 색상 상수화

---
+++ src/styles/color.ts
@@ -0,0 +1,10 @@
+export default {
+  NAVY_BLUE_001: '#5D69F0',
+
+  BLACK_001: '#333333',
+
+  GRAY_001: '#AEAEAE',
+
+  WHITE_001: '#FFFFFF',
+  WHITE_002: '#FAFAFF',
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import COLOR from 'styles/color';
+
+export const theme = {
+  color: {
+    primary: COLOR.NAVY_BLUE_001,
+    onPrimary: COLOR.WHITE_001,
+
+    secondary: COLOR.WHITE_002,
+
+    background: COLOR.WHITE_001,
+    onBackground: COLOR.BLACK_001,
+
+    subText: COLOR.GRAY_001,
+  },
+};
