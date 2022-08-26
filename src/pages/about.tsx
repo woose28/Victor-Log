b9b0c5772707025880f7deb671cbe015d742d52a
woose28 <dldntmdsla@naver.com>
2022-08-26 20:47:39 +0900

    feat: 기본 페이지 구현
    - 구현한 페이지 목록
      - 메인 페이지
      - 소개 페이지
      - 게시글 목록 페이지

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import { Layout } from 'components';
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <span>소개 페이지</span>
+    </Layout>
+  );
+};
+
+export default AboutPage;
+
+export const Head = () => <title>Victor Log | 소개 페이지</title>;
---
+++ src/pages/post.tsx
@@ -0,0 +1,13 @@
+import { Layout } from 'components';
+
+const PostPage = () => {
+  return (
+    <Layout>
+      <span>게시글 목록 페이지</span>
+    </Layout>
+  );
+};
+
+export default PostPage;
+
+export const Head = () => <title>Victor Log | 게시글 목록</title>;
