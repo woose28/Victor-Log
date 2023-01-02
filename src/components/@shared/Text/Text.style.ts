5af83ee5d95d92616b9d0ff78b89746f9af9de39
woose28 <dldntmdsla@naver.com>
2023-01-03 01:30:41 +0900

    refactor: Text 컴포넌트 컨벤션에 맞게 수정
    - fontSizeMapper 객체 Text.style 파일로 분리
    - TextProps type 선언문으로 선언

---
+++ src/components/@shared/Text/Text.style.ts
@@ -0,0 +1,17 @@
+import { FontSize } from 'styles/type';
+
+const fontSizeMapper: Record<FontSize, number> = {
+  10: 0.625,
+  11: 0.6875,
+  12: 0.75,
+  14: 0.875,
+  16: 1,
+  20: 1.25,
+  24: 1.5,
+  32: 2,
+  40: 2.5,
+  48: 3,
+  70: 4.375,
+};
+
+export { fontSizeMapper };
