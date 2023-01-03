e399d6acb63edf1d3e087babb5d900bd4026c091
woose28 <dldntmdsla@naver.com>
2023-01-04 00:59:04 +0900

    feat: PickType 맵드 타입 구현
    - 객체 타입에서 특정 키의 타입을 반환하는 타입이 빈번히 사용되기 때문에 맵드 타입으로 구현

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,5 @@
-export type MappedKeyToUnion<T> = T extends { [key: string]: infer K } ? K : never;
+type MappedKeyToUnion<T> = T extends { [key: string]: infer K } ? K : never;
+
+type PickType<T extends { [key: string]: unknown }, U extends keyof T> = Pick<T, U>[U];
+
+export { MappedKeyToUnion, PickType };
