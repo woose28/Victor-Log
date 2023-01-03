94436a4f846dfc390c9f6c1ba9ac183fbb66ea16
woose28 <dldntmdsla@naver.com>
2023-01-04 00:59:48 +0900

    feat: convertToRem 유틸 함수 구현
    - px 값을 rem으로 변환하는 유틸 함수 구현

--- src/styles/type.ts
+++ src/styles/type.ts
@@ -1,8 +1,12 @@
 import { MappedKeyToUnion } from 'types/utils';
 import COLOR from 'styles/color';
 
-export type AvailableColor = MappedKeyToUnion<typeof COLOR>;
+type AvailableColor = MappedKeyToUnion<typeof COLOR>;
 
-export type FontSize = 10 | 11 | 12 | 14 | 16 | 20 | 24 | 32 | 40 | 48 | 70;
+type FontSize = 10 | 11 | 12 | 14 | 16 | 20 | 24 | 32 | 40 | 48 | 70;
 
-export type FontWeight = 'normal' | 'bold' | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+type FontWeight = 'normal' | 'bold' | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+
+type ConvertPxToRem = (px: number) => number;
+
+export { AvailableColor, FontSize, FontWeight, ConvertPxToRem };
---
+++ src/styles/util.ts
@@ -0,0 +1,5 @@
+import { ConvertPxToRem } from 'styles/type';
+
+const convertPxToRem: ConvertPxToRem = (px) => px / 16;
+
+export { convertPxToRem };
