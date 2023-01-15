34d5565831367705a30cdd54885c1b172b119b9c
woose28 <dldntmdsla@naver.com>
2023-01-15 12:50:49 +0900

    refactor: Footer 컴포넌트 organisms 디렉터리로 이동

---
+++ src/components/organisms/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import * as Styled from './Footer.style';
+
+const Footer = () => {
+  return (
+    <Styled.Wrapper>
+      <Styled.NameText>Victor</Styled.NameText>
+      <Styled.CopyRightText>© Victor. All Rights Reserved.</Styled.CopyRightText>
+    </Styled.Wrapper>
+  );
+};
+
+export default Footer;
