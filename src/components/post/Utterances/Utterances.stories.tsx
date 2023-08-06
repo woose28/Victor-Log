1878ce115790d2c245a78df2defd72b38aeba331
woose28 <dldntmdsla@naver.com>
2023-08-06 21:40:28 +0900

    feat: Utterances 컴포넌트 구현
    - Utterances를 통해 댓글을 작성하는 기능 구현

---
+++ src/components/post/Utterances/Utterances.stories.tsx
@@ -0,0 +1,8 @@
+import Utterances from './Utterances';
+
+export default {
+  title: 'post/Utterances',
+  component: Utterances,
+};
+
+export const DefaultUtterances = () => <Utterances />;
---
+++ src/components/post/Utterances/useUtterances.ts
@@ -0,0 +1,55 @@
+import { useLayoutEffect, useState, useRef } from 'react';
+import { useCreateElement } from 'hooks/useCreateElement/useCreateElement';
+
+type Status = 'pending' | 'failed' | 'success';
+
+const SCRIPT_ATTRIBUTES = {
+  src: 'https://utteranc.es/client.js',
+  repo: process.env.GATSBY_REPOSITORY as string,
+  'issue-term': 'pathname',
+  theme: 'github-light',
+  crossorigin: 'anonymous',
+  async: 'true',
+};
+
+const INITIAL_STATUS = 'pending';
+const SUCCESS_STATUS = 'success';
+const ERROR_STATUS = 'failed';
+
+const useUtterances = () => {
+  const [status, setStatus] = useState<Status>(INITIAL_STATUS);
+
+  const isLoading = status === INITIAL_STATUS;
+  const isSuccess = status === SUCCESS_STATUS;
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const script = useCreateElement({
+    tagName: 'script',
+    handlers: {
+      load: {
+        listener: () => {
+          setStatus(SUCCESS_STATUS);
+        },
+      },
+      error: {
+        listener: () => {
+          setStatus(ERROR_STATUS);
+        },
+      },
+    },
+    attributes: SCRIPT_ATTRIBUTES,
+  });
+
+  useLayoutEffect(() => {
+    containerRef.current?.appendChild(script);
+  }, []);
+
+  return {
+    isLoading,
+    isSuccess,
+    containerRef,
+  };
+};
+
+export { useUtterances };
