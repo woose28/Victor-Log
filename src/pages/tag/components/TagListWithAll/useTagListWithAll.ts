e808867722435e510d144661e673da5becd08330
woose28 <dldntmdsla@naver.com>
2023-10-03 16:35:20 +0900

    fix: TagPage의 All 태그 게시글 개수 계산 로직 수정

--- src/pages/tag/components/TagListWithAll/useTagListWithAll.ts
+++ src/pages/tag/components/TagListWithAll/useTagListWithAll.ts
@@ -1,21 +1,13 @@
-import type { Tag } from './TagListWithAll';
-
 type UseTagListWithAllProps = {
-  tags: Tag[];
   currentTag: string | null;
 };
 
 const ALL_TAG_NAME = 'All';
 
-export const useTagListWithAll = ({ tags, currentTag }: UseTagListWithAllProps) => {
-  const allPostCount = tags.reduce((acc, tag) => acc + tag.count, 0);
-
-  const allTag = { name: ALL_TAG_NAME, count: allPostCount };
-
+export const useTagListWithAll = ({ currentTag }: UseTagListWithAllProps) => {
   const selectedTag = currentTag === null ? ALL_TAG_NAME : currentTag;
 
   return {
-    allTag,
     selectedTag,
   };
 };
